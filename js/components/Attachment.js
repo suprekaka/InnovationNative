2829803e230764bbcddbc85c42e72272b4514872
Kaka <khan0001@synchronoss.com>
2017-03-23 16:23:52 +0800

    navigator's style in web (reverted from commit deb163f0f8dfd12c259df871988c47730e7f0cb1)

--- js/components/Attachment.js
+++ js/components/Attachment.js
@@ -122,9 +122,9 @@ export default class Attachment extends Component {
     }
     return (
       <View style={{ flex: 1 }}>
-        <div style={{height:40,background:'#008cfa',display:'-webkit-box'}}>
+        <div style={{height:60,background:'#008cfa',display:'flex'}}>
           {button}
-          <div  style={{flex:1,margin:'auto',textAlign:'center',padding:'8px'}}><span>{renderScene.title}</span></div>
+          <div  style={{flex:1,margin:'auto',textAlign:'center'}}><span>{renderScene.title}</span></div>
         </div>
         {view}
       </View>
