6131d7e1e86024d13819becd34b6bc8777385619
Kaka <khan0001@synchronoss.com>
2017-03-23 16:24:07 +0800

    web navigator (reverted from commit dec0fdd23a5155419365ead0c796425b8ee766c8)

--- js/App.js
+++ js/App.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import createStore from './store'
 import AttachmentView from './components/AttachmentView'
-import Attachment from './components/Attachment'
 import { service } from './middlewares/webtop'
 import { host } from './config'
-import {Platform} from 'react-native'
 
 service.URL = host
 
@@ -15,18 +13,10 @@ const initialState = {
 const store = createStore(initialState)
 global.store = store
 
-const App = () => {
-  if (Platform.OS === 'web') {
-    return <Provider store={store}>
-      <Attachment />
-    </Provider>
-  } else {
-    return <Provider store={store}>
-      <AttachmentView />
-    </Provider>
-  }
-}
-  
-
+const App = () => (
+  <Provider store={store}>
+    <AttachmentView />
+  </Provider>
+)
 
 export default App
